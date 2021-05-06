9a84753db07796cecb42e3656dcfc56194971253
Prajwal Shenoy <prajwalshenoy07@gmail.com>
2021-05-06 14:17:51 +0530

    Revert "Jyo_1st update"

--- FoodOrderingApp-api/src/test/java/com/upgrad/FoodOrderingApp/api/controller/AddressControllerTest.java
+++ FoodOrderingApp-api/src/test/java/com/upgrad/FoodOrderingApp/api/controller/AddressControllerTest.java
@@ -1,4 +1,4 @@
-/*package com.upgrad.FoodOrderingApp.api.controller;
+package com.upgrad.FoodOrderingApp.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.upgrad.FoodOrderingApp.api.model.AddressList;
@@ -433,5 +433,3 @@ public class AddressControllerTest {
         assertNull(statesLists.getStates());
     }
 }
-
- */
